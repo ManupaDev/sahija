50f1bd51a29e818cab171fc844a6fdc2c339d795
ManupaDev <manupadevsuite@gmail.com>
2023-03-15 10:24:55 +0530

    feat:added nested routing fir categories and subcategoies

--- app/shop/clothing/[category]/[subcategory]/page.tsx
+++ app/shop/clothing/[category]/[subcategory]/page.tsx
@@ -2,11 +2,9 @@ import client from "@/lib/sanity";
 
 async function SubCategoryPage({ params }: { params: { category: string; subcategory: string } }) {
   const { category, subcategory } = params;
-  let data = [];
-  const query = `*[_type == "clothing" && category == "${category}" && clothType == "${subcategory}" ] {id,name,category,clothType,price,quantity}`;
-  data = await client.fetch(query)
-
-  console.log(data);
+  // const query = `*[_type == "clothing" && category == "${category}" && clothType == "${subcategory}" ] {id,name,category,clothType,price,quantity}`;
+  // const data = await client.fetch(query)
+  
 
   return (
     <div className="border border-black px-4 py-4">
