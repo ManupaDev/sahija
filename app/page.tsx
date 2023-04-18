4c5c07afaf0a62d2ce45f9b7e64e5234775d4c6a
ManupaDev <manupadevsuite@gmail.com>
2023-04-19 00:22:22 +0530

    feat:tested sanity integration

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,12 @@
-export default function Home() {
+import client from "@/lib/sanity";
+import { log } from "console";
+
+export default async function Home() {
+  
+  const query = `*[_type == "clothing"] {name,price,category,subcategory}`;
+  const clothing = await client.fetch(query);
+  log(clothing);
+  
   return (
     <div></div>
   )
--- lib/sanity.ts
+++ lib/sanity.ts
@@ -1,10 +1,10 @@
 const sanityClient = require('@sanity/client');
 
 const client = sanityClient({
-  projectId: 'nahru1dy',
+  projectId: '5pg5pxzc',
   dataset: 'production',
   apiVersion: '2023-04-18', // use current UTC date - see "specifying API version"!
-  token: '', // or leave blank for unauthenticated usage
+  token: 'skdIGSPxOWzknj23mUVycmd5bG2JkXzUrcagwyB2ZfRwWpeXDIysfzo0A24ZiQeZunXYxpmB4TfexRLdwBnNt6Vdpu691LtTHW1WsSArJbsWo735SvNe1L2CnLDr3hyM8eDkkHHkHh9LBSVR5A0L84S5S666OcI7RhaCcDrd4hsGZybgo9Tp', // or leave blank for unauthenticated usage
   useCdn: true, // `false` if you want to ensure fresh data
 })
 
